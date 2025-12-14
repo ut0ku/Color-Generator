25dce2bcc302883132b7168c75740158ceb1ab70
ㅤuuto <ivanmukhinofficial@gmail.com>
2025-12-14 05:14:49 +0300

    Change build to IIFE for CSP compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/Color-Generator/',
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'iife'
+      }
+    }
+  }
 })
